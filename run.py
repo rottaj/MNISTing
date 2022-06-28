05359400e52236f1e51ec903626e83124f101897
rottaj <jackrotta12@gmail.com>
2022-06-28 13:48:33 -0400

    Updated

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
+import os
 import numpy as np
 import torch
-from torch.utils.data import DataLoader
+from torch import nn
 import idx2numpy
 import matplotlib.pyplot as plt
 
@@ -25,8 +26,35 @@ print(f"Device of tensor", x1.device)
 #print(test_images)
 #print(test_labels)
 
+class BabyNet(nn.Module):
+  def __init__(self):
+    super(BabyNet, self).__init__()
+    self.flatten = nn.Flatten()
+    self.linear_relu_stack = nn.Sequential(
+      nn.Linear(28*28, 512),
+      nn.ReLU(),
+      nn.Linear(512, 512),
+      nn.ReLU(),
+      nn.Linear(512, 10),
+    )
+  def forward(self, x):
+    x = self.flatten(x)
+    logits = self.linear_relu_stack(x)
+    return logits
 
 
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model = BabyNet().to(device)
+print(model)
+
+X = torch.rand(1, 28,28, device=device)
+logits = model(X)
+pred_probab = nn.Softmax(dim=1)(logits)
+y_pred = pred_probab.argmax(1)
+print("X: ", X, len(X))
+print("Predicted class: ", y_pred)
+
+'''
 i =0
 while i <= 5:
   img = test_images[i].squeeze()
@@ -39,3 +67,4 @@ while i <= 5:
 
   plt.show()
   i+=1
+ '''
